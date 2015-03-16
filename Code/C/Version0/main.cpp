8c2de3a43c56f9185e5b85059831d2861a14bb52
Jamie Taylor <jamie@gaprogman.com>
2015-03-16 18:53:21 +0000

    Began adding test code
    Began adding code to test the constructors, accessors and mutators for
    both classes.

--- Code/C/Version0/main.cpp
+++ Code/C/Version0/main.cpp
@@ -3,22 +3,61 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+/**
+ * @brief Used to instanciate a UserClass object with one Medication object, then call the ToString() method on both
+ */
+void testMedicoClasses() {
 	Medication* ptr;
-	ptr = new Medication("Full Medical Name", "Short Name", "Perscribed Dosage",
-						  "Maximum Dosage", "Initial Dosage Time", "Description",
-						  "Guid", 0);
+	ptr = new Medication("Cocodemol", "Pain Meds", "2 pills, with food, every 4 hours",
+						  "No more than 8 pills per day", "09:30",
+						  "Oval shaped, grey pills with a 'C' on one side",
+						  "Guid", 240);
 	
 	vector<Medication> medsVec;
 	medsVec.clear();
 	medsVec.push_back(*ptr);
 	
 	UserClass* usr;
-	usr = new UserClass("thing", "another thing", medsVec);
+	usr = new UserClass("gaProgMAn", "Jay Tay", medsVec);
 	
-	cout << "UserId:\t" << usr->UserId() << endl;
-	cout << "Medications -> Short Name:\t" << usr->Medications("Guid").ShortName() << endl;
+	cout << usr->ToString() << endl;
+}
+
+void instanciateMedicationClass(UserClass* usr) {
+	Medication* med = new Medication();
 	
+	string fullName;
+	cout << "Please enter the full name of your medication: ";
+	getline(cin, fullName);
+	med->FullMedicalName(fullName);
+	
+}
+
+UserClass* instanciateUserClass() {
+	UserClass *usr = new UserClass();
+	
+	string userName;
+	cout << "Please enter your user name: ";
+	getline(cin, userName);
+	
+	string userId;
+	cout << "Please enter your user ID: ";
+	getline(cin, userId);
+	
+	usr->UserName(userName);
+	usr->UserId(userId);
+	
+	return usr;
+}
+
+void instanciateMedicoClasses() {
+	UserClass* usr = instanciateUserClass();
+	cout << usr->ToString() << endl;
+}
+
+int main(int argc, char **argv)
+{	
+	//testMedicoClasses();
+	instanciateMedicoClasses();
 	return 0;
 }
